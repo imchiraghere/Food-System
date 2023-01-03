82598eefc47f8daa8c31333f92a301df20c3ce44
chirag <imchiraghere@gmail.com>
2023-01-03 22:02:36 +0530

    Adding Some Files

--- routes/food.js
+++ routes/food.js
@@ -9,6 +9,31 @@ router.get("/foodface", function (req, res) {
   res.render("foodface", { message: "" });
 });
 
+///////////////// ----------> Food Add Items Inertface <-----------------\\\\\\\\\\\\\
+
+router.get("/foodadd", function (req, res) {
+  res.render("foodadd", { message: "" });
+});
+
+///////////////// ----------> Food Add Items  <-----------------\\\\\\\\\\\\\
+
+router.post("/foodinsert", function (req, res) {
+  pool.query(
+    "insert into fooditem(itemname, price, typeid) values (?,?,?)",
+    [req.body.item, req.body.price, req.body.type],
+    function (error, result) {
+      if (error) {
+        console.log(error);
+        res.render("foodadd", { message: "Server Error" });
+      } else {
+        res.render("foodadd", {
+          message: "Food Add Successfully!!",
+        });
+      }
+    }
+  );
+});
+
 ///////////////// ----------> Fetch Food Type by DB <-----------------\\\\\\\\\\\\\
 
 router.get("/fetchfoodtype", function (req, res) {
@@ -97,7 +122,7 @@ router.post("/foodsubmit", upload.single("pic"), function (req, res) {
 
 router.get("/fooddisplay", function (req, res) {
   pool.query(
-    "select fd.*,(select T.typename from foodtype T where T.typeid=fd.type) as type, (select I.itemname from fooditem I where I.itemid=fd.item) as fooditem from food_details fd",
+    "select fd.*,(select T.typename from foodtype T where T.typeid=fd.foodtype) as type, (select I.itemname from fooditem I where I.itemid=fd.fooditemname) as fooditem, (select OT.offtypename from offertype OT where OT.offtypeid=fd.offertype) as offertype, (select o.offname from offer o where o.offid=fd.offer) as offer from food_details fd",
     function (error, result) {
       if (error) {
         console.log(error);
@@ -107,6 +132,62 @@ router.get("/fooddisplay", function (req, res) {
       }
     }
   );
+});
+
+////////////////// ---------> Search Food By Id <------------\\\\\\\\\\\\\\
+
+router.get("/searchbyid", function (req, res) {
+  pool.query(
+    "select fd.*,(select T.typename from foodtype T where T.typeid=fd.foodtype) as type, (select I.itemname from fooditem I where I.itemid=fd.fooditemname) as fooditem, (select OT.offtypename from offertype OT where OT.offtypeid=fd.offertype) as offertype, (select o.offname from offer o where o.offid=fd.offer) as offer from food_details fd where foodid=?",
+    [req.query.fid],
+    function (error, result) {
+      if (error) {
+        console.log(error);
+        res.render("foodmodify", { data: [], message: "Server Error" });
+      } else {
+        res.render("foodmodify", { data: result[0], message: "Successfull!!" });
+      }
+    }
+  );
+});
+
+///////////////////-----------> Food Modification (Edit / Delete) <--------------\\\\\\\\\\\\\\\
+
+router.post("/food_edit_del", function (req, res) {
+  if (req.body.button == "Edit") {
+    pool.query(
+      "update food_details set foodtype=?, fooditemname=?, offertype=?, offer=?, price=? where foodid=?",
+      [
+        req.body.type,
+        req.body.item,
+        req.body.offertype,
+        req.body.offer,
+        req.body.price,
+        req.body.foodid,
+      ],
+      function (error, result) {
+        if (error) {
+          console.log(error);
+          res.redirect("/food/fooddisplay");
+        } else {
+          res.redirect("/food/fooddisplay");
+        }
+      }
+    );
+  } else {
+    pool.query(
+      "delete from food_details where foodid=?",
+      [req.body.foodid],
+      function (error, result) {
+        if (error) {
+          console.log(error);
+          res.redirect("/food/fooddisplay");
+        } else {
+          res.redirect("/food/fooddisplay");
+        }
+      }
+    );
+  }
 });
 
 module.exports = router;
